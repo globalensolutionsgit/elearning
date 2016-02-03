2e2a86084178aba7e75a0cfda35143d4cb4c4a5b
kalaiR <kalaimca.gs@gmail.com>
2016-02-03 13:26:30 -0800

    update custom 404 page using htaccess

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-mysql_select_db('A978313_lms',mysql_connect('mysql509.ixwebhosting.com','A978313_lms','A978313_lms'))or die(mysql_error());
+// mysql_select_db('A978313_lms',mysql_connect('mysql509.ixwebhosting.com','A978313_lms','A978313_lms'))or die(mysql_error());
 //mysql_select_db('testlms',mysql_connect('localhost','root',''))or die(mysql_error());
-//mysql_select_db('A978313_lms',mysql_connect('localhost','root','root'))or die(mysql_error());
+mysql_select_db('A978313_lms',mysql_connect('localhost','root',''))or die(mysql_error());
 ?>
